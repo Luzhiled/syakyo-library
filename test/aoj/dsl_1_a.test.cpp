bb869b499bb619240c7c5eeb90191963ae018c67
Luzhiled <Luzhiled+github@gmail.com>
2023-06-20 08:00:22 +0900

    fix int-alias, dsu; append test dsl_1_a

---
+++ test/aoj/dsl_1_a.test.cpp
@@ -0,0 +1,31 @@
+// verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/problems/DSL_1_A
+
+#include "src/cpp-template/header/int-alias.hpp"
+#include "src/data-structure/disjoint-set-union.hpp"
+
+#include <iostream>
+
+namespace luz {
+
+  void main_() {
+    usize n, q;
+    std::cin >> n >> q;
+
+    DisjointSetUnion d(n);
+    while (q--) {
+      usize com, x, y;
+      std::cin >> com >> x >> y;
+
+      if (not com) {
+        d.merge(x, y);
+      } else {
+        std::cout << (d.same(x, y)) << std::endl;
+      }
+    }
+  }
+
+} // namespace luz
+
+int main() {
+  luz::main_();
+}
