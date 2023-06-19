d8c3c20c9291d7499f75e127d3861edc5bdd9e27
Luzhiled <Luzhiled+github@gmail.com>
2023-06-20 08:30:50 +0900

    append test grl_6_a

---
+++ test/aoj/grl_6_a.test.cpp
@@ -0,0 +1,29 @@
+// verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/problems/GRL_6_A
+
+#include "src/cpp-template/header/int-alias.hpp"
+#include "src/cpp-template/header/size-alias.hpp"
+#include "src/graph/flow/max-flow.hpp"
+
+#include <iostream>
+
+namespace luz {
+
+  void main_() {
+    usize n, m;
+    std::cin >> n >> m;
+
+    MaxFlowGraph< u32 > g(n);
+    while (m--) {
+      usize u, v;
+      u32 c;
+      std::cin >> u >> v >> c;
+      g.add_directed_edge(u, v, c);
+    }
+    std::cout << g.max_flow(0, n - 1) << std::endl;
+  }
+
+} // namespace luz
+
+int main() {
+  luz::main_();
+}
