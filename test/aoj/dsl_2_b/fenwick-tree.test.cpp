52cc9733d2a722b1e5caf639cdf4663900f1bcb3
Luzhiled <Luzhiled+github@gmail.com>
2023-06-20 08:03:59 +0900

    fix fenwick-tree; append test dsl_2_b

---
+++ test/aoj/dsl_2_b/fenwick-tree.test.cpp
@@ -0,0 +1,32 @@
+// verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/problems/DSL_2_B
+
+#include "src/cpp-template/header/int-alias.hpp"
+#include "src/cpp-template/header/size-alias.hpp"
+#include "src/data-structure/fenwick-tree.hpp"
+
+#include <iostream>
+
+namespace luz {
+
+  void main_() {
+    usize n, q;
+    std::cin >> n >> q;
+
+    FenwickTree< u32 > ft(n);
+    while (q--) {
+      usize com, x, y;
+      std::cin >> com >> x >> y;
+
+      if (not com) {
+        ft.add(x - 1, y);
+      } else {
+        std::cout << ft.sum(x - 1, y) << std::endl;
+      }
+    }
+  }
+
+} // namespace luz
+
+int main() {
+  luz::main_();
+}
