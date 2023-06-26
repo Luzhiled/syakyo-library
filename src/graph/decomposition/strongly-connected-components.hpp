f8774c0a9e208af52609730fb447bd2a6439fbe4
Luzhiled <Luzhiled+github@gmail.com>
2023-06-27 00:36:31 +0900

    update scc

--- src/graph/decomposition/strongly-connected-components.hpp
+++ src/graph/decomposition/strongly-connected-components.hpp
@@ -1,89 +1,59 @@
 #pragma once
 
-#include "src/cpp-template/header/size-alias.hpp"
-
 #include <vector>
+#include <cstddef>
 
 namespace luz {
 
-  template < class G >
-  class StronglyConnectedComponents {
-    using graph     = G;
-    using cost_type = typename graph::cost_type;
+  struct SCCGraph {
+    using usize = std::size_t;
+    using graph = std::vector< std::vector< usize > >;
 
-    using groups_type = std::vector< std::vector< usize > >;
-
-    graph g;
     usize n;
+    graph g;
 
-    std::vector< usize > low, ord, visited, group_id;
+    std::vector< usize > S, B, I;
 
-    usize ord_cnt, group_cnt;
+    void dfs(usize v, graph &scc) {
+      B.emplace_back(I[v] = S.size());
+      S.emplace_back(v);
 
-    void dfs(usize v) {
-      low[v] = ord[v] = ord_cnt++;
-      visited.emplace_back(v);
-
-      for (auto &e: g[v]) {
-        if (ord[e.to] == n) {
-          dfs(e.to);
-          low[v] = std::min(low[v], low[e.to]);
-        } else {
-          low[v] = std::min(low[v], ord[e.to]);
+      for (auto u: g[v]) {
+        if (not I[u]) {
+          dfs(u, scc);
+          continue;
         }
+        while (I[u] < B.back()) B.pop_back();
       }
 
-      if (low[v] != ord[v]) return;
+      if (I[v] != B.back()) return;
 
-      while (true) {
-        usize u = visited.back();
-        visited.pop_back();
-
-        ord[u]      = n + 1;
-        group_id[u] = group_cnt;
-
-        if (u == v) break;
+      scc.emplace_back();
+      B.pop_back();
+      while (I[v] < S.size()) {
+        scc.back().emplace_back(S.back());
+        I[S.back()] = n + scc.size();
+        S.pop_back();
       }
-
-      group_cnt++;
     }
 
    public:
-    explicit StronglyConnectedComponents(const graph& g_)
-        : g(g_), n(g.size()), low(n), ord(n, n), group_id(n), ord_cnt(0), group_cnt(0) {
-      visited.reserve(n);
+    SCCGraph(usize n) : n(n), g(n) {}
 
+    void add_directed_edge(usize from, usize to) {
+      g[from].emplace_back(to);
+    }
+
+    graph get_scc_list() {
+      graph scc;
+      I.assign(n, 0);
+      S = B = {};
       for (usize v = 0; v < n; v++) {
-        if (ord[v] != n) continue;
-        dfs(v);
+        if (not I[v]) dfs(v, scc);
       }
-
-      for (auto &id: group_id) {
-        id = group_cnt - id - 1;
-      }
+      return {scc.rbegin(), scc.rend()};
     }
 
-    groups_type groups() const {
-      std::vector< usize > counts(group_cnt);
-      for (usize i = 0; i < n; i++) {
-        counts[group_id[i]]++;
-      }
-
-      groups_type groups(group_cnt);
-      for (usize i = 0; i < group_cnt; i++) {
-        groups[i].reserve(counts[i]);
-      }
-
-      for (usize i = 0; i < n; i++) {
-        groups[group_id[i]].emplace_back(i);
-      }
-
-      return groups;
-    }
-
-    std::vector< usize > group_ids() const {
-      return group_id;
-    }
   };
 
 } // namespace luz
